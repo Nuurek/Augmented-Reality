422fd8745bad70abe7d14b4c616f2fa1ad9bbcca
Jakub Nurski <nurskijakub@gmail.com>
2016-11-22 22:49:03 +0100

    Added video writer to save video output.

--- Augmented Reality/Source.cpp
+++ Augmented Reality/Source.cpp
@@ -8,6 +8,9 @@ const unsigned int FRAME_WIDTH = 640;
 const unsigned int FRAME_HEIGHT = 480;
 const char * WINDOW_NAME = "Camera capture";
 
+const bool WRITE_VIDEO = true;
+const char * WRITE_FILENAME = "capture.mpeg";
+
 
 int exitWithError(const char * errorMessage) {
 	std::cerr << errorMessage << "\n";
@@ -23,11 +26,27 @@ int main(int argc, char** argv) {
 
 	cv::namedWindow(WINDOW_NAME);
 
+	auto videoWriter = cv::VideoWriter();
+	if (WRITE_VIDEO) {
+		videoWriter.open(
+			WRITE_FILENAME, cv::VideoWriter::fourcc('P', 'I', 'M', '1'), FPS, cv::Size(FRAME_WIDTH, FRAME_HEIGHT)
+		);
+
+		if (!videoWriter.isOpened()) {
+			return exitWithError("Couldn't create video writer.");
+		}
+	}
+
 	while (true) {
 		cv::Mat frame;
 		camera >> frame;
+
+		if (WRITE_VIDEO) {
+			videoWriter.write(frame);
+		}
+
 		cv::imshow(WINDOW_NAME, frame);
-		if (cv::waitKey(1) >= 0) {
+		if (cv::waitKey(1000 / FPS) >= 0) {
 			return EXIT_SUCCESS;
 		}
 	}
