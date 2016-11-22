28fc3db55ce764cfe038a09cb81116a4d01c548b
Jakub Nurski <nurskijakub@gmail.com>
2016-11-22 22:37:42 +0100

    Test of camera.

--- Augmented Reality/Source.cpp
+++ Augmented Reality/Source.cpp
@@ -1,23 +1,34 @@
-#include "opencv2/core/core.hpp"
-#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/opencv.hpp"
+
+
 #include <iostream>
 
-using namespace cv;
-using namespace std;
+const unsigned int FPS = 30;
+const unsigned int FRAME_WIDTH = 640;
+const unsigned int FRAME_HEIGHT = 480;
+const char * WINDOW_NAME = "Camera capture";
+
+
+int exitWithError(const char * errorMessage) {
+	std::cerr << errorMessage << "\n";
+	return EXIT_FAILURE;
+}
 
 int main(int argc, char** argv) {
-	Mat image;
-	image = imread("data/lena.jpg", IMREAD_COLOR); // Read the file
+	auto camera = cv::VideoCapture(0);
 
-	if (!image.data) // Check for invalid input
-	{
-		cout << "Could not open or find the image" << std::endl;
-		return -1;
+	if (!camera.isOpened()) {
+		return exitWithError("Couldn't open camera.");
 	}
 
-	namedWindow("Display window", WINDOW_AUTOSIZE); // Create a window for display.
-	imshow("Display window", image); // Show our image inside it.
+	cv::namedWindow(WINDOW_NAME);
 
-	waitKey(0); // Wait for a keystroke in the window
-	return 0;
+	while (true) {
+		cv::Mat frame;
+		camera >> frame;
+		cv::imshow(WINDOW_NAME, frame);
+		if (cv::waitKey(1) >= 0) {
+			return EXIT_SUCCESS;
+		}
+	}
 }
