f1d6ba1044c3d3363ea62099d199b5a64e1c93d8
Jakub Nurski <nurskijakub@gmail.com>
2016-12-11 18:21:05 +0100

    Initial recover after merge.

--- Augmented Reality/KeyManager.cpp
+++ Augmented Reality/KeyManager.cpp
@@ -6,6 +6,10 @@ std::unordered_map<char, std::string> KeyManager::keyToOptionsMap={
 	{ GLFW_KEY_2, "subRegions" },
 	{ GLFW_KEY_3, "edgels" },
 	{ GLFW_KEY_4, "lineSegments" },
+	{ GLFW_KEY_5, "mergedLineSegments" },
+	{ GLFW_KEY_6, "extendedLineSegments" },
+	{ GLFW_KEY_7, "lineSegmentsWithCorner" },
+	{ GLFW_KEY_8, "markers" },
 	{ GLFW_KEY_ESCAPE, "escape" },
 	{ GLFW_KEY_P, "poseFinderExample" }
 };
--- Augmented Reality/KeyManager.h
+++ Augmented Reality/KeyManager.h
@@ -4,23 +4,13 @@
 #include <GLFW/glfw3.h>
 
 class KeyManager {
-	std::unordered_map<std::string, bool> options;
-	std::unordered_map<char, std::string> keyToOptionsMap{
-		{'1', "regions"},
-		{'2', "subRegions"},
-		{'3', "edgels"},
-		{'4', "lineSegments"},
-		{'5', "mergedLineSegments"},
-		{'6', "extendedLineSegments"},
-		{'7', "lineSegmentsWithCorner"},
-		{'8', "markers"}
-	};
-
 public:
 	static void init(GLFWwindow* window);
 	static void keyPressed(char keyCode);
 	bool isActive(std::string option);
 	static void key_callback(GLFWwindow * window, int key, int scancode, int action, int mods);
 	void handleEvents();
+	static std::unordered_map<std::string, bool> options;
+	static std::unordered_map<char, std::string> keyToOptionsMap;
 };
 
